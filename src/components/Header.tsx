3f666a7b9c7fde0c615f5881333003742b5f1f22
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2024-12-18 19:40:08 +0900

    fix: 카테고리 순서 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,10 +26,10 @@ const Header = () => {
               <Link href="/champions">챔피언 목록</Link>
             </li>
             <li>
-              <Link href="/items">아이템 목록</Link>
+              <Link href="/rotation">챔피언 로테이션</Link>
             </li>
             <li>
-              <Link href="/rotation">챔피언 로테이션</Link>
+              <Link href="/items">아이템 목록</Link>
             </li>
             <li>
               <DarkMode />
